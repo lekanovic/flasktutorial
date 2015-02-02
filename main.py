9a58f2859c75626dec99a5673fad07092085631a
Radovan Lekanovic <lekanovic@gmail.com>
2015-02-01 19:00:16 -0800

    Add a new page when the user is logged in.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, views
+from flask import Flask, render_template, request, views, redirect, url_for
 from flask.ext.mongokit import MongoKit
 from flask.ext.bootstrap import Bootstrap
 import time, json, requests, urllib
@@ -8,14 +8,26 @@ app = Flask(__name__)
 #mongo = MongoKit(app)
 bootstrap = Bootstrap(app)
 
+class Userhome(views.MethodView):
+
+    def get(self):
+        return render_template('userhome.html',username="Radovan")
+
+    def post(self):
+        pass
+
+
 class Register(views.MethodView):
 
     def get(self):
         return render_template('register.html')
 
     def post(self):
-        pass
-
+        username = request.form['Username']
+        email = request.form['Email address']
+        passwd = request.form['Password']
+        print "%s %s %s" % (username,email,passwd)
+        return redirect(url_for('userhome'))
 
 class Main(views.MethodView):
 
@@ -23,7 +35,7 @@ class Main(views.MethodView):
         return render_template('index.html', price=self.btstamp())
 
     def post(self):
-        pass
+        return "POST"
 
     def getDollarPrice(self):
         url = "http://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json"
@@ -43,6 +55,7 @@ class Main(views.MethodView):
 
 main_view = Main.as_view('index')
 register_view = Register.as_view('register')
+userhome_view = Userhome.as_view('userhome')
 
 app.add_url_rule('/',
                  view_func=main_view,
@@ -54,6 +67,10 @@ app.add_url_rule('/index.html',
 
 app.add_url_rule('/register.html',
                  view_func=register_view,
+                 methods=["GET", "POST"])
+
+app.add_url_rule('/userhome.html',
+                 view_func=userhome_view,
                  methods=["GET", "POST"])
 
 app.debug = True
